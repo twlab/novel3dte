bfd96190b3f42b60b5fb2bd4ac0beca15fe74494
karaquaid <31666696+karaquaid@users.noreply.github.com>
2026-08-24 10:39:37 -0400

    Update find_functional_loops.py
    Added annotations

--- find_functional_loops.py
+++ find_functional_loops.py
@@ -1,76 +1,81 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jan  7 10:05:30 2020
+Finds locations of CTCF binding peaks within <threshold> bp of a loop anchor site of interest and returns
+a table with the loop name, which side of the loop the anchor of interest is on, and a string with the 
+locations of competing CTCFs
 
-@author: Kara
+Usage: python3 find_competing_ctcfs.py <loopname_file> <ctcf_peak_file> <loop_location_file> <threshold> <output filename>
+
+@author: 15017
 """
 
 import sys
 
-#reads a bed file of promoters into a dictionary.
-def readbed(filename):
-    i=0
-    dictname = {}
-    with open(filename) as bedfile:
-        for line in bedfile:
-            sline = line.rstrip().split()
-            chrom = sline[0]
-            start=int(sline[1])
-            end=int(sline[2])
-            dictname[i]=[chrom,start,end]
-            i+=1
-    return dictname
-        
-def find_cre(chromosome,position,cre_dict,threshold,side,bidirectional):#mess around with minval and maxval
-    foundstuff={}
-    if side=="right":
-        maxval=position+threshold
-        if bidirectional ==True:
-            minval=position - threshold
-        else:
-            minval=position
-    else:
-        if bidirectional == True:
-            maxval=position + threshold
-        else: 
-            maxval=position
-        minval=position-threshold
-    foundone=False
-    i=0
-    for key in cre_dict:
-        cre=cre_dict[key]
-        cre_chrom=cre[0]
-        if cre_chrom==chromosome:
-            cre_start=cre[1]
-            if cre_start >= minval:
-                cre_end = cre_dict[key][2]
-                #print("%s\t%s\t%s"%(cre_chrom,cre_start,cre_end))
-                if cre_start<=maxval:
-                    foundone=True
-                    foundstuff[i]=[cre_chrom,cre_start,cre_end]
-                    i+=1
-    return foundone,foundstuff
-                    
-    
-#################MAIN SCRIPT###################################################
+#this function takes a single loop name, the dictionary of loop locations, the 
+#list of ctcf chromosomes, list of the end positions of ctcfs, and the threshold distance,
+#and returns a string with the locations of competing ctcf peaks in the format:
+#Lx1,Lx2,... where x1 is the distance (bp) from the loop anchor of the first competing
+#ctcf, x2 is the distance for the second competing ctcf, etc. 
+def searchleft(loopname,loopdict,cchrs,cends,threshold):
+    #This function finds ctcfs that are within the threshold distance from the 
+    #left end of the loop anchor
+    lch = loopdict[loopname][0]
+    lstart = loopdict[loopname][1]
+    conflicts = "LNone"
+    firstconflict = 1
+    min_end = lstart - threshold
+    for i in range(len(cends)):
+        if cchrs[i] == lch:
+            if (cends[i] > min_end) & (cends[i] < lstart):
+                if firstconflict == 1:
+                    conflicts = "L%s"%(lstart-cends[i])
+                    firstconflict = 0
+                else:
+                    conflicts += ",L%s"%(lstart-cends[i])                
+    return conflicts
 
-       
-promoter_file="gm_all_promoters.bed"
-enhancer_file="gm_polycomb_repressed.bed"
-promoters = readbed(promoter_file)
-enhancers=readbed(enhancer_file)
+#this function takes a single loop name, the dictionary of loop locations, the 
+#list of ctcf chromosomes, list of the start positions of ctcfs, and the threshold distance,
+#and returns a string with the locations of competing ctcf peaks in the format:
+#Rx1,Rx2,... where x1 is the distance (bp) from the loop anchor of the first competing
+#ctcf, x2 is the distance for the second competing ctcf, etc. 
+def searchright(loopname,loopdict,cchrs,cstarts,threshold):
+    #This function finds ctcfs that are within the threshold distance from the 
+    #right end of the loop anchor
+    lch = loopdict[loopname][0]
+    lend = loopdict[loopname][2]
+    conflicts = "RNone"
+    firstconflict = 1
+    max_start = lend + threshold
+    for i in range(len(cends)):
+        if cchrs[i] == lch:
+            if (cstarts[i] < max_start) & (cstarts[i] > lend):
+                if firstconflict == 1:
+                    conflicts = "R%s"%(cstarts[i] - lend)
+                    firstconflict = 0
+                else:
+                    conflicts += ",R%s"%(cstarts[i] - lend)               
+    return conflicts
+
+##############Main Script##############
+
 loopfile = sys.argv[1]
-loop_loc_file = sys.argv[2]
-threshold = int(sys.argv[3])
-loop_loc_file = "human_loops_re-derived_orthologous.bed"
-threshold = 35000
-bidirectional = True
-#looplist = []#list of loopnames for loops of interest
-#with open(loopfile) as f:
-#    for line in f:
-#        sline = line.rstrip().split()
-#        looplist.append(sline[0])
-loopdict={}
+ctcffile = sys.argv[2]
+loop_loc_file = sys.argv[3]
+threshold = int(sys.argv[4])
+output = sys.argv[5]
+#loopfile = "looplist.txt"
+#ctcffile = "CTCF_peaks.bed"
+#loop_loc_file = "orthologyAnnotated_REderived_human_loop_anchorCTCF.txt"
+#threshold = 50000
+
+looplist = []#list of loopnames for loops of interest
+with open(loopfile) as f:
+    for line in f:
+        sline = line.rstrip().split()
+        looplist.append(sline[0])
+
+loopdict = {}#dictionary with loopnames as keys and values [chr,start,end]
 with open(loop_loc_file) as f:
     for line in f:
         sline = line.rstrip().split()
@@ -78,75 +83,40 @@ with open(loop_loc_file) as f:
         ch = sline[0]
         start = int(sline[1])
         end = int(sline[2])
-        loopdict[loopname] = [ch,start,end,loopname]
+        side = sline[5]
+        if loopname in looplist:
+            newname = loopname + "_" + side
+            loopdict[newname] = [ch,start,end,loopname,side]
 
-candidates_dict={}
-cres_dict={}
-looplengths=[]
+                
+
+cchrs = []#list of chromosomes for ctcf peaks
+cstarts = [] #start positions for ctcf peaks
+cends = []#end positions for ctcf peaks
+with open(ctcffile) as f:
+    firstline = 1
+    for line in f:
+        if firstline == 1:
+            firstline = 0
+        else:
+            sline = line.rstrip().split()
+            cchrs.append(sline[0])
+            cstarts.append(int(sline[1]))
+            cends.append(int(sline[2]))
+
+
+conflict_dict = {}
+#make a dictionary with loopnames as keys, and strings with the positions of competing
+#ctcfs as values
 for key in loopdict:
-    right_promoters={}
-    right_enhancers={}
-    left_promoters={}
-    left_enhancers={}
-    left_anchor=int(loopdict[key][1])
-    right_anchor=int(loopdict[key][2])
-    chromosome=loopdict[key][0]
-    loop_length=right_anchor-left_anchor
-    looplengths.append(loop_length)
-        #check for promoters within 10kb of right anchor
-    right_promoter,right_promoters=find_cre(chromosome,right_anchor,promoters,threshold,"right",bidirectional)
-    if right_promoter==True:
-        #if found, check for enhancers within 10kb of left anchor
-         left_enhancer,left_enhancers=find_cre(chromosome,left_anchor,enhancers,threshold,"left",bidirectional)
-         if left_enhancer==True:
-            #if found, add loop to dictionary of candidates
-            candidates_dict[key]=loopdict[key]
-    #check for promoters within 10kb of left anchor
-    left_promoter,left_promoters=find_cre(chromosome,left_anchor,promoters,threshold,"left",bidirectional)
-    if left_promoter==True:
-        #if found, check for enhancers within 10kb of right anchor
-        right_enhancer,right_enhancers=find_cre(chromosome,right_anchor,enhancers,threshold,"right",bidirectional)
-        if right_enhancer==True:
-            #if found, add loop to dictionary of candidates
-            if key not in candidates_dict.keys():
-                candidates_dict[key]=loopdict[key]
-    cres_dict[key]=[right_promoters,left_enhancers,left_promoters,right_enhancers]
-                    
-
-with open("human_all_enhancer_promoter_candidates_10kb.txt","w") as nf:
-    nf.write("loop_name\tchromosome\tstart\tend\t\n")
-    for key in candidates_dict:
-        nf.write("%s\t%s\t%s\t%s\t\n"%(key,candidates_dict[key][0],candidates_dict[key][1],candidates_dict[key][2]))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    l_conflicts = searchleft(key,loopdict,cchrs,cends,threshold)
+    r_conflicts = searchright(key,loopdict,cchrs,cstarts,threshold)
+    all_conflicts = l_conflicts +","+ r_conflicts
+    conflict_dict[key] = [all_conflicts,loopdict[key][3],loopdict[key][4]]
+    
+#create a tab-delimited file with columns: loopname, re-derived_side, competing ctcf string
+with open(output,"w") as nf:
+    nf.write("loop_name\tre_side\tconflicting_ctcf\n")
+    for key in conflict_dict:
+        nf.write("%s\t%s\t%s\n"%(conflict_dict[key][1],conflict_dict[key][2],conflict_dict[key][0]))
 
